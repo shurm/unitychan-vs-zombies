6623e272918763e14cb514e06fab9fa94940cbcd
mas868 <mas868@scarletmail.rutgers.edu>
2018-05-26 18:20:47 -0400

    added head collider to unitychan

--- Assets/Camera Scripts/MouseAimCamera.cs
+++ Assets/Camera Scripts/MouseAimCamera.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using System.Collections;
 
-public class MouseAimCamera : MonoBehaviour {
+public class MouseAimCamera : MonoBehaviour
+{
 	public GameObject target;
 	public float rotateSpeed = 5;
-	Vector3 offset;
+
+    public bool turnDirection = true;
+
+	private Vector3 offset;
+
+    private int multiplier = 1;
 	
 	void Start() {
 		offset = target.transform.position - transform.position;
-	}
+        if (turnDirection)
+            multiplier = 1;
+        else
+            multiplier = -1;
+
+
+    }
 	
 	void LateUpdate() {
-		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed * multiplier;
 		target.transform.Rotate(0, horizontal, 0);
 
 		float desiredAngle = target.transform.eulerAngles.y;
