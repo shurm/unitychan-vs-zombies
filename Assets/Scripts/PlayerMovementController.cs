f2ca121917ec82a72410b3300631579330872695
mas868 <mas868@scarletmail.rutgers.edu>
2018-06-04 16:30:59 -0400

    added combos to unity animator controller

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -22,54 +22,54 @@ public class PlayerMovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
-            anim.SetBool("hp", true);
-        else
-            anim.SetBool("hp", false);
-        if (Input.GetMouseButtonDown(1))
-            anim.SetBool("hk", true);
-        else
-            anim.SetBool("hk", false);
-
-        //attack animations happen a little slower (looks better)
-        if (IsAttacking())
-            anim.speed = 0.7f;
-        else
-            anim.speed = 1f;
-
-
-        if (Input.GetKey(KeyCode.LeftShift))
-            run = true;
-        else
-            run = false;
-
-        float direction = Input.GetAxis("Horizontal");
-
-        float speed = Input.GetAxis("Vertical");
-        if (speed < 0)
-            speed = 0;
-
-        anim.SetFloat("Speed", speed);
-        anim.SetFloat("Direction", direction);
-        anim.SetBool("run", run);
-
-        float moveX = direction * 100f * Time.deltaTime;
-
-        //if player is running, player travels more distance (aka travels at higher velocity)
-        if (run)
         {
-            moveX *= 1.5f;
-            currentSpeed = runSpeed;
+            anim.SetBool("Punch", true);
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("Kick", true);
         }
         else
-            currentSpeed = walkSpeed;
+        {
+            anim.SetBool("Punch", false);
+            anim.SetBool("Kick", false);
 
-        //rotates the player
-        if (direction != 0)
-            transform.Rotate(0, moveX, 0, Space.World);
-       
-        if (speed > 0)
-            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftShift))
+                run = true;
+            else
+                run = false;
+
+            float direction = Input.GetAxis("Horizontal");
+
+            float speed = Input.GetAxis("Vertical");
+            if (speed < 0)
+                speed = 0;
+
+            anim.SetFloat("Speed", speed);
+            anim.SetFloat("Direction", direction);
+            anim.SetBool("run", run);
+
+            float moveX = direction * 100f * Time.deltaTime;
+
+            //if player is running, player travels more distance (aka travels at higher velocity)
+            if (run)
+            {
+                moveX *= 1.5f;
+                currentSpeed = runSpeed;
+            }
+            else
+                currentSpeed = walkSpeed;
+
+            //rotates the player
+            if (direction != 0)
+                transform.Rotate(0, moveX, 0, Space.World);
+
+            if (speed > 0)
+                transform.position += transform.forward * currentSpeed * Time.deltaTime;
+        }
     }
 
     public bool IsAttacking()
