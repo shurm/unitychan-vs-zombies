6672e9cea25f0a0d510de29f342bed4747ade2a5
mas868 <mas868@scarletmail.rutgers.edu>
2018-06-23 01:29:35 -0400

    added selecting scene functionality for main menu scene

---
+++ Assets/ScenePreviewLoader.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScenePreviewLoader : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class ImageDescriptionPair
+    {
+        public string description;
+        public Sprite previewImage;
+    }
+
+
+    public ImageDescriptionPair[] sceneAndDescriptions;
+    public Image previewSign;
+
+    public Text descriptionLabel;
+
+    private int index = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetPreviewAndDescription();
+    }
+
+    private void SetPreviewAndDescription()
+    {
+        descriptionLabel.text = sceneAndDescriptions[index].description;
+        previewSign.sprite = sceneAndDescriptions[index].previewImage;
+    }
+	
+    public void LeftArrowClick()
+    {
+        index = (index - 1) % sceneAndDescriptions.Length;
+        SetPreviewAndDescription();
+    }
+
+    public void RightArrowClick()
+    {
+        index = (index + 1) % sceneAndDescriptions.Length;
+        SetPreviewAndDescription();
+    }
+
+    public void LoadCurrentScene()
+    {
+        string sceneName = descriptionLabel.text.ToLower();
+
+        for (int a = 0; a < SceneManager.sceneCount; a++)
+        {
+            Scene scene = SceneManager.GetSceneAt(a);
+            string lowerCaseSceneName = scene.name.ToLower();
+            if (scene.name.Equals(lowerCaseSceneName))
+            {
+                // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+                SceneManager.LoadScene(a, LoadSceneMode.Single);
+                Debug.Log("loading scene");
+               
+            }
+           
+        }
+        
+        
+    }
+}
