a01451d18310f039b13033b2d985d970d5da45a4
mas868 <mas868@scarletmail.rutgers.edu>
2018-06-23 22:34:43 -0400

    loading bar displays for scene loading

--- Assets/ScenePreviewLoader.cs
+++ Assets/ScenePreviewLoader.cs
@@ -7,6 +7,11 @@ using UnityEngine.UI;
 
 public class ScenePreviewLoader : MonoBehaviour
 {
+    public GameObject loadingUI;
+    public Transform loadingProgbar;
+    public Text loadingText;
+    private readonly float LOAD_READY_PERCENTAGE = 0.9f;
+    AsyncOperation sceneAO;
 
     [System.Serializable]
     public class ImageDescriptionPair
@@ -49,22 +54,60 @@ public class ScenePreviewLoader : MonoBehaviour
 
     public void LoadCurrentScene()
     {
-        string sceneName = descriptionLabel.text.ToLower();
+        string desiredSceneName = descriptionLabel.text.ToLower();
 
-        for (int a = 0; a < SceneManager.sceneCount; a++)
+        Debug.Log("SceneManager.sceneCount is " + SceneManager.sceneCountInBuildSettings);
+        for (int a = 0; a < SceneManager.sceneCountInBuildSettings; a++)
         {
-            Scene scene = SceneManager.GetSceneAt(a);
-            string lowerCaseSceneName = scene.name.ToLower();
-            if (scene.name.Equals(lowerCaseSceneName))
+            //Scene scene = SceneManager.GetSceneByBuildIndex(a);
+            string path = SceneUtility.GetScenePathByBuildIndex(a);
+            string sceneName = path.Substring(0, path.Length - 6).Substring(path.LastIndexOf('/') + 1);
+
+            string lowerCaseSceneName = sceneName.ToLower();
+            Debug.Log("lowerCaseSceneName is  " + lowerCaseSceneName);
+            if (desiredSceneName.Equals(lowerCaseSceneName))
             {
                 // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-                SceneManager.LoadScene(a, LoadSceneMode.Single);
-                Debug.Log("loading scene");
-               
+                ChangeScene(a);
+                Debug.Log("starting " + sceneName);
+
             }
-           
         }
+       
+    }
+
+    private void ChangeScene(int sceneIndex)
+    {
+        loadingUI.SetActive(true);
         
-        
+        StartCoroutine(LoadingSceneRealProgress(sceneIndex));
+    }
+
+    IEnumerator LoadingSceneRealProgress(int sceneIndex)
+    {
+        yield return new WaitForSeconds(1);
+
+        sceneAO = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+
+        // disable scene activation while loading to prevent auto load
+        sceneAO.allowSceneActivation = false;
+
+        while (!sceneAO.isDone)
+        {
+            loadingProgbar.localScale = new Vector3(sceneAO.progress, 1, 1) ;
+            loadingText.text = sceneAO.progress.ToString("0") + "%";
+
+            if (sceneAO.progress >= LOAD_READY_PERCENTAGE)
+            {
+                loadingProgbar.localScale = Vector3.one;
+                loadingText.text = "100%";
+               
+                 sceneAO.allowSceneActivation = true;
+                
+            }
+            Debug.Log(sceneAO.progress);
+            yield return null;
+        }
     }
 }
+
